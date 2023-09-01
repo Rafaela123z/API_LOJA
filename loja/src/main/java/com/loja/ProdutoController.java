42f98484b3cf653c15e5d853de7d8d8e0e0c9d36
Rafaela <rafaela.barros2@aluno.senai.br>
2023-09-01 08:58:59 -0300

    Criando endpoint que busca um produto pelo ID

---
+++ loja/src/main/java/com/loja/ProdutoController.java
@@ -0,0 +1,29 @@
+package com.loja;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/loja/produtos")
+public class ProdutoController {
+    private TabelaProdutos tabelaProdutos;
+
+    public ProdutoController() {
+        this.tabelaProdutos = new TabelaProdutos();
+    }
+@GetMapping
+    public List<Produto> buscarTodosOsProdutosDaLoja() {
+        return this.tabelaProdutos.buscarTodosOsProdutos();
+}
+
+@GetMapping("/{produtoId}")
+    public Produto buscarProdutoPeloIdNaLoja(@PathVariable int produtoId){
+        Produto produtoProcurado = this.tabelaProdutos.buscarProdutoPeloId(produtoId);
+        return produtoProcurado;
+}
+}
